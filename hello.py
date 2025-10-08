977eb6ac87dfa9f9d808c2f4ec0757d2128e9a5a
Hades <kbchwalek@gmail.com>
2025-10-08 16:43:39 -0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,33 @@
+
+#This program says hello and asks for my name.
+print('Hello World')
+
+print('What is your name')  #ask for their name
+myName = input()
+
+print('The length of your name is')
+      
+print(len(myName))
+      
+print('What is your age?')  #ask for their age
+
+myAge = input()
+      
+print('You will be ' + str(int(myAge)+1) + ' in a year')
+
+print(myName, 'if we add the number of letters in your name to your age:',)
+print('Then you will be,')
+
+mart = len(myName)
+
+bart = int(myAge)
+
+print (mart+bart)
+
+print ('In',len(myName), 'years') 
+
+
+
+
+
+      
